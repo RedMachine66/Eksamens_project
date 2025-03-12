5bb1769ec479f4f8fd5f9dc22f252c811038ae98
Magnus Bakke <eucmpau001@eucnordvest.dk>
2025-03-12 11:14:56 +0100

    Auth hashing

--- auth.py
+++ auth.py
@@ -5,37 +5,43 @@ import db_utils, hashlib
 
 db_path = 'dashcorp.db'
 
-def auth():
-    while True:
-        print("Please enter your email to log-in or sign-up.")
-        user_email=input()
-        con=connect_to_db(db_path)
-        if email_exists(con, user_email):
-            while True:
-                logged_in=auth_login(con, user_email)
-                if logged_in:
-                    return True
-        else:
-            print("Your email is not associated with an existing account, do you want to create one? (y/n)")
-            user_create_account=input()
-            if user_create_account == "y":
-                account_created=auth_create_user(con, user_email)
-                if account_created:
-                    return True
-
-
-
-def auth_login(con, user_email):
+def login(con, hashed_user_email):
     print("please enter your password")
-    user_password=input()
-    if check_password(con, user_email, user_password):
+    hashed_user_password=auth_hash(input())
+    if db_utils.check_password(con, hashed_user_email, hashed_user_password):
         return True
     else:
         print("Incorrect password")
         return False
 
-def auth_create_user(con, user_email):
+def create_account(con, user_email):
     print("Enter new password")
-    new_user_password=input()
-    create_user(con, user_email, new_user_password)
+    new_hashed_user_password=auth_hash(input())
+    db_utils.create_user(con, user_email, new_hashed_user_password)
     return True
+
+def auth_hash(input):
+    encoded_input=input.encode("utf-8")
+    hash=hashlib.sha256(encoded_input)
+    return hash.hexdigest()
+
+def auth():
+    con=db_utils.connect_to_db(db_path)
+    print(con)
+    while True:
+        print("Please enter your email to log-in or sign-up.")
+        hashed_user_email=auth_hash(input())
+        print(hashed_user_email)
+        con=db_utils.connect_to_db(db_path)
+        if db_utils.email_exists(con, hashed_user_email):
+            while True:
+                logged_in=auth_login(con, hashed_user_email)
+                if logged_in:
+                    return True
+        else:
+            print("Your email is not associated with an existing account, do you want to create one? (y/n)")
+            should_create_account=input()
+            if should_create_account == "y":
+                create_account=create_account(con, hashed_user_email)
+                if create_account:
+                    return True
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import auth, physics_calculator, source_finder, sys
 
 print("Welcome to student tools by Dashcorp!")
-authorized=auth()
+authorized=auth.auth()
 if authorized == False:
     print("Access denied")
     sys.exit("Exiting program")
