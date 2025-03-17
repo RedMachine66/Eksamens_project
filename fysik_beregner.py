52b43a1f7d91a4a57f9af65e34e0c366da11fcc2
Rasmus <rasmusmadsen2010@gmail.com>
2025-03-17 12:59:22 +0100

    Lavet fysik beregner og gjort så der ikke kan være negative tal og ingen bogstaver der kan indtastes

--- fysik_beregner.py
+++ fysik_beregner.py
@@ -10,7 +10,7 @@ def calculate_projectile(start_speed , angle_degrees):
     flight_time = (2 * start_speed * math.sin(angle)) / g
 
     # Calculate maximum height
-    max_height = (start_speed**2 * math.sin(angle)**2) / (2 * g)
+    max_height = int(start_speed**2 * math.sin(angle)**2) / (2 * g)
 
     # Calculate range
     range_distance = (start_speed**2 * math.sin(2 * angle)) / g
@@ -20,9 +20,25 @@ def calculate_projectile(start_speed , angle_degrees):
     print(f"Maximum height: {max_height:.2f} meters")
     print(f"Range: {range_distance:.2f} meters")
 
-# User input
-v0 = float(input("Enter start speed (m/s): "))
-angle = float(input("Enter angle (degrees): "))
+# User input and ensure user input i valid 
+while True:
+    try:
+        v0 = float(input("Enter start speed (m/s): "))
+        if v0 <= 0:
+            print("speed must be a positive number!")
+        break
+    except ValueError:
+        print("enter a valid number")
+
+while True:
+    try:
+        angle = float(input("Enter angle (degrees): "))
+        if angle <= 0 or angle >= 180:
+            print("angle must be a positive number or a number there is not over 180 degress!")
+        else:
+            break #exit the loop if input is valid 
+    except ValueError:
+        print("Enter a numeric number")
 
 # Call the function
 calculate_projectile(v0, angle)
