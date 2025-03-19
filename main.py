de7c94391483ff408d26efc974ce939511c52025
Magnus Bakke <eucmpau001@eucnordvest.dk>
2025-03-19 10:28:23 +0100

    Fixed physics calculator imlpementation

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import auth, physics_calculator, source_finder, sys, db_utils
+import auth, physics_calc, source_finder, sys, db_utils
+
 
 print("Welcome to student tools by Dashcorp!")
 authorized=auth.auth()
@@ -6,3 +7,22 @@ if authorized == False:
     print("Access denied")
     sys.exit("Exiting program")
 print("Welcome to Dashcorp!")
+
+while True:
+    print("Enter 'c' to use the physics calculator, 's' to use the source finder, or 'x' to close the program")
+    userinput=input()
+    print(userinput)
+    if userinput == 'x':
+        sys.exit("Exiting program")
+    if userinput == 'c':
+        while True:
+            print("Enter 'b' to go back")
+            print("Enter a start speed value to calculate projectile movement")
+            userinput_calculator=input()
+            if userinput_calculator == 'b':
+                break
+            physics_calc.get_input_and_calculate()
+    if userinput == 's':
+        print("The source finder is not implemented yet")
+    if userinput not in ('x','c','s'):
+        print("Enter a valid input")
